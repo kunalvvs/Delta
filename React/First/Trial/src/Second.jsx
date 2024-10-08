5b8b6217af97cd49d271b66b67af5044c0ead0fc
kunalvvs <kunalvvs@gmail.com>
2024-10-08 13:49:24 +0530

    save

---
+++ React/First/Trial/src/Second.jsx
@@ -0,0 +1,15 @@
+let name = "HI Man";
+
+function Second()
+  
+{
+    return <>
+        <button ><h1 id="ccc">Button</h1></button>
+        {name}
+          <div>hwllo</div>
+    </>
+}
+
+
+
+export default Second
