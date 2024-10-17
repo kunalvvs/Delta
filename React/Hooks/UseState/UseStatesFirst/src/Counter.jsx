db1a57545d84907d5ca7590aca4f4f658b998d5d
kunalvvs <kunalvvs@gmail.com>
2024-10-17 12:50:59 +0530

    count

--- React/Hooks/UseState/UseStatesFirst/src/Counter.jsx
+++ React/Hooks/UseState/UseStatesFirst/src/Counter.jsx
@@ -9,6 +9,11 @@ export default function Counter()
     return <>
     
     <h3 style={{border:"2px solid grey",borderRadius:"8px"}}> Counter value is {count}</h3>
+
+    <button onClick={() => setCount(count + 1)}>
+                Click me (Second method to define onClick)
+            </button> 
+
     <button style={{border:"2px solid yellow" }} onClick={Inc}>Add this</button>
     </>
 }
