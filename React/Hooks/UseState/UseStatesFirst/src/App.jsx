fedee089f9d0ecb78909a527cd9ade27523c8991
kunalvvs <kunalvvs@gmail.com>
2024-11-02 13:43:14 +0530

    #

--- React/Hooks/UseState/UseStatesFirst/src/App.jsx
+++ React/Hooks/UseState/UseStatesFirst/src/App.jsx
@@ -5,11 +5,8 @@ import LikeButton from './LikeButton'
 import PasswordGenerator from './PasswordGenerator' //this is cpy pst cde
 
 
-
-
 function App() {
   
-
   return (
     <>
      <h2 style={{color:"orange"}}>Lets Start The Hooks First Topic is UseState</h2>
