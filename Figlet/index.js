f56452432f4bf9cdf0c14f9c7af56fd1ebeaf4eb
kunalvvs <kunalvvs@gmail.com>
2024-11-05 22:26:49 +0530

    nodejs completed

--- Figlet/index.js
+++ Figlet/index.js
@@ -1,6 +1,6 @@
 var figlet = require("figlet");
 
-figlet("Thanks Dinu Bhai", function (err, data) {
+figlet("Hacked", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
--- NodeJs/Fifth(import)/script.js
+++ NodeJs/Fifth(import)/script.js
@@ -1,3 +1,5 @@
 import {sum,PI} from "./math.js";
+import { generate, count } from "random-words";
 
+console.log(generate());
 console.log(sum(2,4));
