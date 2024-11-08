cd472e40fdee147482a102ac769b221befdd6bfe
kunalvvs <kunalvvs@gmail.com>
2024-11-08 22:46:49 +0530

    request,response and route in express

--- Express/First/index.js
+++ Express/First/index.js
@@ -7,4 +7,31 @@ let port = 3000;
 
 app.listen(port, ()=> {
     console.log(`app is listening on port ${port}`)
-})
+});
+
+
+// app.use((req,res) => {
+//     console.log("request received");
+
+//     // res.send("This is response");
+//     res.send("<h1>Fruits</h1>");
+// }); //here semicolon is optional
+
+app.post(('/') ,(req,res)=> {
+    res.send("Connected with the Post root");
+});
+
+app.get(('/') ,(req,res)=> {
+    res.send("Connected with the root");
+});
+
+app.get(('/a') ,(req,res)=> {
+    res.send("Connected with the a");
+});
+app.get(('/b') ,(req,res)=> {
+    res.send("Connected with the b");
+});
+
+app.get(('*') ,(req,res)=> {
+    res.send("Page not found");
+});
---
+++ Express/First/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+> {
+//     console.log("request received");
+
+//     // res.send("This is response");
+//     res.send("<h1>Fruits</h1>");
+// }); //here semicolon is optional
