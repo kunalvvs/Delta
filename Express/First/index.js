c84465a386b2c6936d1ee3afaf3447238c761e60
kunalvvs <kunalvvs@gmail.com>
2024-11-09 15:08:41 +0530

    Express Basic Completed

--- Express/First/index.js
+++ Express/First/index.js
@@ -10,10 +10,10 @@ app.listen(port, ()=> {
 });
 
 
-// app.use((req,res) => {
+// app.use((req,res) => {            //request
 //     console.log("request received");
 
-//     // res.send("This is response");
+//     // res.send("This is response");      //response
 //     res.send("<h1>Fruits</h1>");
 // }); //here semicolon is optional
 
@@ -22,16 +22,35 @@ app.post(('/') ,(req,res)=> {
 });
 
 app.get(('/') ,(req,res)=> {
-    res.send("Connected with the root");
+    res.send("Connected with the root path");
 });
 
-app.get(('/a') ,(req,res)=> {
-    res.send("Connected with the a");
-});
-app.get(('/b') ,(req,res)=> {
-    res.send("Connected with the b");
+
+
+// app.get(('/a') ,(req,res)=> {    //routing
+//     res.send("Connected with the a");
+// });
+// app.get(('/b') ,(req,res)=> {
+//     res.send("Connected with the b");
+// });
+
+// app.get(('*') ,(req,res)=> {
+//     res.send("Page not found");
+// });
+
+
+app.get("/:username/:id" ,(req,res)=>{    //Path parameter
+    let {username,id} = req.params;
+    let htmls = `<h1>Welcome to the @${username}</h1>`;
+    res.send(htmls);
 });
 
-app.get(('*') ,(req,res)=> {
-    res.send("Page not found");
-});
+app.get("/search", (req,res) =>{
+    let {q} = req.query;
+    console.log(req.query);
+    if(!q)
+    {
+        res.send("Nothing searched");
+    }
+    res.send(`No result about ${q}`);
+})
