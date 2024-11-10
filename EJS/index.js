d4771135d2c8985c918214fdb7cae9180f32f772
kunalvvs <kunalvvs@gmail.com>
2024-11-10 17:19:59 +0530

    EJS started

---
+++ EJS/index.js
@@ -0,0 +1,14 @@
+//EJS(Embedded Javascript Templates)
+
+const express = require("express");
+
+let app = express();
+
+const port =  8080;
+
+app.listen( port , () => {
+    console.log("listening from the port ");
+}) 
+
+
+
