f363f627c951ee232c0c028a82b4231a39683339
kunalvvs <kunalvvs@gmail.com>
2024-11-14 17:26:44 +0530

    EJS Created

--- EJS/index.js
+++ EJS/index.js
@@ -1,13 +1,21 @@
 //EJS(Embedded Javascript Templates)
 
 const express = require("express");
+const path = require("path");
 
 let app = express();
 
 const port =  8080;
 
+app.set("view engine",'ejs');
+app.set("views",path.join(__dirname,"path"))
+
+app.get('/',(req,res) =>{
+    res.render("home.ejs");
+})
+
 app.listen( port , () => {
-    console.log("listening from the port ");
+    console.log(`listening from the port ${port} `);
 }) 
 
 
