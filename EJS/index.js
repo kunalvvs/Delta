3bd0155a6a54ac5c3422b6f6848f4485bfd74061
kunalvvs <kunalvvs@gmail.com>
2024-11-14 18:28:37 +0530

    added views directory to set path

--- EJS/index.js
+++ EJS/index.js
@@ -1,18 +1,20 @@
 //EJS(Embedded Javascript Templates)
 
 const express = require("express");
-const path = require("path");
+const path = require("path");  //with this can run the server from the another path(parent path)
 
 let app = express();
 
 const port =  8080;
 
 app.set("view engine",'ejs');
-app.set("views",path.join(__dirname,"path"))
+app.set("views",path.join(__dirname,"/views"));   //__dirname identify the current directory of the index.js
 
 app.get('/',(req,res) =>{
-    res.render("home.ejs");
+    res.render("home.ejs");  //render is used to redirect the views/ejsfile.ejs
 })
+
+
 
 app.listen( port , () => {
     console.log(`listening from the port ${port} `);
