8bcd3c74f6fcec517108bdd0083c1d20db4eadfd
kunalvvs <kunalvvs@gmail.com>
2024-11-22 15:00:40 +0530

    Restful Done

--- REST/First/index.js
+++ REST/First/index.js
@@ -1,4 +1,4 @@
-const exp = require("constants");
+// const exp = require("constants");
 const express = require("express");
 const app = express();
 const port = 8080;
@@ -8,9 +8,8 @@ const { v4: uuidv4 } = require('uuid');
  const methodOverride = require('method-override');
 
 
-
 app.use(express.urlencoded({extended:true})); //when POST used for submit the form or data
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 
 app.set("view engine","ejs");
@@ -18,8 +17,8 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 
 app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
 
 let posts= [
     {id:uuidv4(),username:"apnacollege",content:"This is the first post"},
@@ -31,19 +30,19 @@ let posts= [
 
 app.get("/posts",(req,res)=>{
     res.render("index",{posts});  //for render a all post route(index.ejs)
-})
+});
 
 
 app.get("/posts/new",(req,res)=>{
     res.render("new");  //for render the create post route(new.ejs)
-})
+});
 
 app.post("/posts",(req,res)=>{  //to send req of new post to adding in the posts route
     let {username,content} = req.body; //destructure , req.body supported in POST
     let id = uuidv4();
     posts.push({id,username,content});
     res.redirect("posts");
-})
+});
 
 app.get("/posts/:id",(req,res)=>{      
     let {id} = req.params;
@@ -51,7 +50,7 @@ app.get("/posts/:id",(req,res)=>{
     res.render("show.ejs" , {post})
 });
 
-app.patch("posts/:id",(req,res)=>{    //edit the post
+app.patch("/posts/:id",(req,res)=>{    //edit the post
     let {id} = req.params;
     let newContent = req.body.content;
     let post = posts.find((p) => id === p.id );
@@ -60,13 +59,21 @@ app.patch("posts/:id",(req,res)=>{    //edit the post
     console.log(post);
     
     
-})
+});
 
 
-app.get("/posts/:id/edit",(req,res)=>{     //get with edt route
+app.get("/posts/:id/edit",(req,res)=>{     //get with edit route
     let {id} = req.params;
     let post = posts.find((p) => id === p.id );
     res.render("edit",{post});
 
   
-})
+});
+
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+     posts = posts.filter((p) => id !== p.id );
+     res.redirect("/posts");
+    
+});
