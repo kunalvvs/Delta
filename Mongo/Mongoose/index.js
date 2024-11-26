933b8bd2b8d9c56ebaca73ce735b097ee6b8854d
kunalvvs <kunalvvs@gmail.com>
2024-11-26 22:02:14 +0530

    mongoose

--- Mongo/Mongoose/index.js
+++ Mongo/Mongoose/index.js
@@ -1,3 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+main().then( ()=>{
+    console.log('connected to db');  //promise resolve
+}).catch(err => console.log(err));  //if error occured 
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+}
+
+
+const userSchema = new mongoose.Schema({  //Schema 
+    name: String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model("User",userSchema);  //collection created
+
+const user1 = new User({
+    name: "Kunal",
+    email: "kunal@gmail.com",
+    age: 25,
+});   //document created in the users collections
+
+const user2 = new User({
+    name: "Rohan",
+    email:"Rohan@gmail.com",
+    age: 30,
+});
+
+user2.save().then((res) =>{   //save in the DB
+    console.log(res);
+}).catch((err)=>{
+    console.log(err);
+});
