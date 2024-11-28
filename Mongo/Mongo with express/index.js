de98f683d90db2eb5304df9ae7fd4951e4975dae
kunalvvs <kunalvvs@gmail.com>
2024-11-28 22:23:32 +0530

    Whatsapp chat first route created

--- Mongo/Mongo with express/index.js
+++ Mongo/Mongo with express/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
+
+app.set("view engine",'ejs');
+app.set("views",path.join(__dirname,"/views")); 
+app.use(express.static(path.join(__dirname,"public")));  //public folder 
 
 main().then(()=>{
     console.log("Connection successfull")
@@ -10,12 +16,31 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
-const port = 8080;
 
+const port = 8080;
 app.listen(port,(req,res)=>{
     console.log(`Server is running on port ${port}`)
 });
 
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("root is working");
+})
+
+
+let chat1 = new Chat({
+    from:"Neha",
+    to:"Rahul",
+    msg:"Hello",
+    created_at: new Date()  //it gives UTC format
+});
+
+// chat1.save().then(res=>{
+//     console.log(res);
+// }); 
+
+app.get("/chats",(req,res)=>{  //index route
+    Chat.find().then(data=>{
+        // console.log(data);
+        res.render("index.ejs",{data});
+        })
 })
---
+++ Mongo/Mongo with express/init.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main().then(()=>{
+    console.log("Connection successfull")
+}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let allChats = [
+    {
+        from:"Neha",
+        to:"Rahul",
+        msg:"I think i can write anything here",
+        created_at: new Date() 
+    },
+    {
+        from:"Martin",
+        to:"Guptil",
+        msg:"Great things in life matters only",
+        created_at: new Date() 
+    },
+    {
+        from:"Dijkat",
+        to:"Diljit",
+        msg:"Hi Dililuminati",
+        created_at: new Date() 
+    },
+    {
+        from:"Amit",
+        to:"Raman",
+        msg:"I am gonna college, whats your plan",
+        created_at: new Date() 
+    }
+];
+
+Chat.insertMany(allChats);
