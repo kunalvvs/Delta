9793733319fb9e45ceab1a976809b4976d08d231
kunalvvs <kunalvvs@gmail.com>
2025-04-30 15:58:51 +0530

    fix: restore console log statement and improve code structure

--- JavaScript/First/app.js
+++ JavaScript/First/app.js
@@ -1,3 +1,24 @@
 a=12;
 b=65;
 console.log(`Additon of ${a} and ${b} is ${a+b}`); //template litrals -> `${ }`
+
+
+
+//truty and falsy value , in JS all are having boolean context
+if(1)
+{
+    console.log("True")
+
+}
+else
+{
+    console.log("False")
+}
+
+//alert and prompt
+
+let name = prompt("Enter the name");
+console.log(`Welcome ${name}`);
+console.log("Welcome with swag "+name);
+
+alert("Hi "+name);
