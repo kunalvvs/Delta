d2f9c94e674dbc33f704d1be8a66552c3326678e
kunalvvs <kunalvvs@gmail.com>
2025-05-02 18:04:54 +0530

    fix: enhance array.js with additional console logs; update index.html to include slice, splice, and sort methods; modify style.css for improved layout

--- JavaScript/First/array.js
+++ JavaScript/First/array.js
@@ -2,7 +2,7 @@
 let arr = [23,42,32,"Hello"];
 console.log("OG array",arr);
 
-console.log("Array Started : \n",arr[3]);
+console.log("index 3  : ",arr[3]);
 
 
 arr[4] = ["322"];
@@ -18,7 +18,7 @@ arr.unshift("First");
 console.log("After unShift",arr);
 
 
-//indexOf and includes
+//indexOf and includes:- true/false
 console.log("-----------------------------------");
 console.log("Index is ",arr.indexOf(42));
 
@@ -29,3 +29,16 @@ let ar2 = ["ferrari","nano"];
 console.log("-----------------------------------");
 console.log("Concatenation: ",ar1.concat(ar2));
 console.log("Reverse array 1",ar1.reverse());
+
+//slice method
+console.log("-----------------------------------");
+console.log("Slice method : ", ar1.slice(1));
+
+//Splice method
+
+console.log("-----------------------------------");
+console.log("Splice method: ",ar2.splice(0,1,"Supra","Lambo"));
+
+//sort method - it convert number into string for sorting
+console.log("-----------------------------------");
+console.log("Sorting: ",ar1.sort());
