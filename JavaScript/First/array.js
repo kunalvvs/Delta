03c2617ed1ef8a925c981b475f4321c1063788bc
kunalvvs <kunalvvs@gmail.com>
2025-05-02 16:35:42 +0530

    fix: enhance array.js with additional console logs and improve index.html structure by adding array methods list

--- JavaScript/First/array.js
+++ JavaScript/First/array.js
@@ -1,5 +1,7 @@
 
 let arr = [23,42,32,"Hello"];
+console.log("OG array",arr);
+
 console.log("Array Started : \n",arr[3]);
 
 
@@ -7,10 +9,23 @@ arr[4] = ["322"];
 
 arr[0]=45;
 
-
+console.log("-----------------------------------");
 arr.push("Last");
 console.log("After push ",arr); //push,pop,shift,unshift
 arr.pop();
 console.log("After Pop",arr);
 arr.unshift("First");
 console.log("After unShift",arr);
+
+
+//indexOf and includes
+console.log("-----------------------------------");
+console.log("Index is ",arr.indexOf(42));
+
+//concat and reverse 
+
+let ar1 = ["audi","bmw"];
+let ar2 = ["ferrari","nano"];
+console.log("-----------------------------------");
+console.log("Concatenation: ",ar1.concat(ar2));
+console.log("Reverse array 1",ar1.reverse());
