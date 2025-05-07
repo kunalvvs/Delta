e7f0e782021c09b2fe3f7e60aa0dfba9b589a00c
kunalvvs <kunalvvs@gmail.com>
2025-05-07 17:08:14 +0530

    fix: update 'Objects' section to 'Objects Literals' in index.html; add Literals.js for object examples

---
+++ JavaScript/First/Literals.js
@@ -0,0 +1,21 @@
+let student = {
+    name : "Kunal",
+    age : 22,
+    marks: 45,
+    color: ["red","yelllow"]
+};
+
+console.log("Objects Literals ",student);
+
+//it is better than array because in the array not 
+// exist key value pair that is more understanding terms
+
+const post = {
+    username: "@kunal",
+    content: "Mission of success",
+    likes: 234,
+    reposts: 5,
+    tags: ["@another","@dinu"]
+}
+
+console.log("My Post ",post);
