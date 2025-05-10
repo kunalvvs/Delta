ea6bd5558174d257931b4d36598b54b3e956d498
kunalvvs <kunalvvs@gmail.com>
2025-05-10 22:19:54 +0530

    #

---
+++ JavaScript/First/Function.js
@@ -0,0 +1,18 @@
+
+function entry()
+{
+    console.log("Function started");
+}
+
+entry();
+
+//function with arguments parameters
+function Info(name, age)
+{
+    console.log("Name: " + name);
+    console.log("Age: " + age);
+    console.log(`Name: ${name}`);
+    console.log(`Age: ${age}`);
+}
+
+Info("John", 25);
---
+++ JavaScript/First/guessing.js
@@ -0,0 +1,2 @@
+//guessing number
+let guess = prompt("Guess a number between 1 and 10");
