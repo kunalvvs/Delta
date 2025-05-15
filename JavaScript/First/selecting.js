78d7e448225548f2f9ddf985f871a2d7c7d4bc61
kunalvvs <kunalvvs@gmail.com>
2025-05-15 17:56:17 +0530

    add: update index.html to include 'DOM, Selectors' in the Array methods section; create selecting.js with various DOM selection methods

---
+++ JavaScript/First/selecting.js
@@ -0,0 +1,9 @@
+document.getElementById("id");
+
+document.getElementsByClassName("");
+
+document.getElementsByTagName("h1");
+
+
+document.querySelector("p");   //it is same from above selectors 
+document.querySelectorAll("p");
