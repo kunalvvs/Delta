217fae24768ac57e3f09fd2dba1deabe4b27bb7c
kunalvvs <kunalvvs@gmail.com>
2025-05-17 11:28:19 +0530

    add: update index.html to enhance the DOM, Selectors section; create selecting.js with various DOM manipulation methods

--- JavaScript/First/selecting.js
+++ JavaScript/First/selecting.js
@@ -7,3 +7,24 @@ document.getElementsByTagName("h1");
 
 document.querySelector("p");   //it is same from above selectors 
 document.querySelectorAll("p");
+
+//these are properties met 
+let para = document.querySelector("p");
+
+para.innerHTML;   
+para.innerText;
+para.textContent; 
+
+//we can manipulate attributes
+let para1 = document.querySelector("p");
+para.getAttribute("id");
+para.setAttribute("id", "myId");
+para.removeAttribute("id");
+
+
+// we can use style property
+
+let links = document.querySelector("h1");
+links.style.color = "red";
+links.style.fontSize = "30px";
+links.style.fontFamily = "Arial";
