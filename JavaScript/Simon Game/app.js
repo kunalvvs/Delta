aaee0d28cd972ed6d4f21b6ebbf804ce1fe77707
kunalvvs <kunalvvs@gmail.com>
2025-05-21 11:02:03 +0530

    fix: correct button flash timing and ensure button press functionality in Simon Game

--- JavaScript/Simon Game/app.js
+++ JavaScript/Simon Game/app.js
@@ -2,9 +2,10 @@
 let gameSeq =[];
 let userSeq =[];
 
-
 let started = false;
 let level =0;
+
+let btns = ["one","two","three","four"];
 
 let h2 = document.querySelector("h2");
 
@@ -16,15 +17,14 @@ document.addEventListener("keypress",function() {
 
         levelUp();
     }
-})
+});
 
 function btnFlash(btn)
 {
     btn.classlist.add("flash");
-
    setTimeout(function(){
     btn.classlist.remove("flash");
-   },1000);
+   },300);
 }
 
 function levelUp()
@@ -32,5 +32,23 @@ function levelUp()
     level++;
     h2.innerText = `Level ${level}`;
 
-    btnFlash();
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    btnFlash(randBtn);
+}
+
+function btnPress(){
+    let btn = this;
+    btnFlash(btn);
+    console.log("button pressed");
+}
+
+let allBtn = document.querySelector(".btn");
+for(btn of allBtn)
+{
+    btn.addEventListener("click",btnPress);
 }
