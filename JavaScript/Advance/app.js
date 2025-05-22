8c82b0a74176d10bb1068f27c084e398d01a303d
kunalvvs <kunalvvs@gmail.com>
2025-05-22 22:53:04 +0530

    add: implement color change functionality using callback hell in app.js

--- JavaScript/Advance/app.js
+++ JavaScript/Advance/app.js
@@ -17,3 +17,22 @@ function three(){
 }
 
 three();
+
+//callback hell or nesting
+
+let h1 = document.querySelector("h1");
+
+function changeColor(color,delay,nextColor)
+{
+    setTimeout(()=>{
+        h1.style.color=color;
+        if (nextColor()) nextColor();
+    },delay);
+    
+}
+
+changeColor("red",1000,()=>{
+    changeColor("blue",1000,()=> {
+        changeColor("green",1000 );
+     });
+});
