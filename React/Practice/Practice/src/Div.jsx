a6e99ca4a6d80912f654ba5bca03b367954c322f
kunalvvs <kunalvvs@gmail.com>
2025-06-25 17:39:39 +0530

    feat: restructure HTML layout and enhance CSS for improved UI interaction

--- React/Practice/Practice/src/Div.jsx
+++ React/Practice/Practice/src/Div.jsx
@@ -6,27 +6,28 @@ export default function Div()
 
     let [one,setOne] = useState(true);
     let [two,setTwo] = useState(false);
+    
     const [isOneVisible, setIsOneVisible] = useState(true);
 
     let handleState=()=>{
         setOne(false);
         setTwo(true);
-    }
+    }  
 
-    const handleToggle = () => {
+    const handleToggle = () => {  
         setIsOneVisible(!isOneVisible);
     };
-    return (
+    return ( 
         <div>
 
-          { one &&  <div className="one"  ></div>}
+          { one &&  <div className="one"  ></div>}  
            { two && <div className="two"></div>}
             <button onClick={handleState}>Toggle</button>
 
 
             <div>
             {isOneVisible ? <div className="one"></div> : <div className="two"></div>}
-            <button onClick={handleToggle}>Toggle</button>
+            <button onClick={handleToggle}>Toggle</button> 
         </div>
         </div>
     )
