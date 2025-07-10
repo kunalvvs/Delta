f58a79e1b88ee5ae6df5840c5ca0cdcde2d975fe
kunalvvs <kunalvvs@gmail.com>
2025-07-10 19:40:56 +0530

    feat: update index.html title and implement todoSlice reducer with add, delete, and mark done functionalities

--- Redux/Redux_First/Todo/src/features/Todo/todoSlice.js
+++ Redux/Redux_First/Todo/src/features/Todo/todoSlice.js
@@ -1,14 +1,35 @@
-
-
 //Reducers
 
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState ={
-
+    todos: [{ id:"abc" ,task:"First Task",isDone:false }]
 }
 
 
 export const todoSlice = createSlice({
-    
+    name:"todo",
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+          const newTodo={
+            id:nanoid(),
+            task:action.payload,
+            isDone:false
+          }
+          state.todos.push(newTodo); //direct mutation than Reactjs
+        },
+        deleteTodos: (state ,action)=>{
+            state.todos = state.todos.filter(todo=>todo.id !== action.payload);
+        },
+        isMarkDone:(state,action)=>{
+            state.todos = state.todos.map(todo=>{
+                if(todo.id === action.payload){
+                    return {...todo,isDone:true}
+                
+        }
+    })}}
 })
+
+export const {addTodo,deleteTodos,isMarkDone}= todoSlice.actions;
+export default todoSlice.reducer;
