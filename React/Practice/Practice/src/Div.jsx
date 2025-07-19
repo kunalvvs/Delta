01fbade7a24ebc9c26520bf0966c891fd777186a
kunalvvs <kunalvvs@gmail.com>
2025-07-19 22:28:05 +0530

    refactor: Simplify state management and improve rendering logic in Div and Other components

--- React/Practice/Practice/src/Div.jsx
+++ React/Practice/Practice/src/Div.jsx
@@ -3,28 +3,14 @@ import './App.css'
 
 export default function Div()
 {
-
-    let [one,setOne] = useState(true);
-    let [two,setTwo] = useState(false);
     
     const [isOneVisible, setIsOneVisible] = useState(true);
-
-    let handleState=()=>{
-        setOne(false);
-        setTwo(true);
-    }  
 
     const handleToggle = () => {  
         setIsOneVisible(!isOneVisible);
     };
     return ( 
         <div>
-
-          { one &&  <div className="one"  >First</div>}  
-           { two && <div className="two">Second</div>}
-            <button onClick={handleState}>Toggle</button>
-
-
             <div>
             {isOneVisible ? <div className="one">First</div> : <div className="two">Second</div>}
             <button onClick={handleToggle}>Toggle</button> 
--- React/Practice/Practice/src/Other.jsx
+++ React/Practice/Practice/src/Other.jsx
@@ -6,8 +6,7 @@ export default function Other()
     
     let [text,setText] = useState();
     let [add,setAdd] = useState();
-    let [div,setDiv] = useState(true);
-    let [div1,setDiv1] = useState(false);
+
 
     let handleText=(evt)=>{
         setText(evt.target.value);
@@ -18,22 +17,14 @@ export default function Other()
         setAdd(text);
     }
 
-    let handleBox = () =>{
-        if(div){
-            setDiv1(true);
-            setDiv(false);
-        }
-    }
 
     return <>
     
     <input type="text" value={text}  placeholder="write something" onChange={handleText}></input>
-    <p > {text} </p>
+    <p > Live Texting :-  {text} </p>
 
     <button onClick={handleBut}>Add</button>
-    <p>{add}</p>
+    <p>  👉 {add}</p>
 
-    <div> {div ? <div className="one">First</div> : <div className="two">Second</div>} </div>
-    <button onClick={handleBox}>Toggle</button>
     </>
 }
