99f527f5f45d7e2478323c7eb100b6076374801a
kunalvvs <kunalvvs@gmail.com>
2025-07-20 13:55:11 +0530

    feat: Add initial HTML, CSS, and JavaScript for image swapping functionality

---
+++ JavaScript/Frontend Domination/First/app.js
@@ -0,0 +1,10 @@
+
+
+let p = document.querySelector("p");
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click",function()
+{
+    p.innerText="Now It is Changed";
+})
---
+++ JavaScript/Frontend Domination/Second/app.js
@@ -0,0 +1,12 @@
+
+
+let f = document.querySelector(".f");
+let s = document.querySelector(".s");
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click",function()
+{
+    s.setAttribute("src","https://cdn.pixabay.com/photo/2024/05/15/20/57/developer-8764524_1280.jpg");
+    f.setAttribute("src","https://tse3.mm.bing.net/th/id/OIP.Ag-nwzIPW2WDlBKp70VBhQHaEp?w=1280&h=804&rs=1&pid=ImgDetMain&o=7&rm=3");
+});
