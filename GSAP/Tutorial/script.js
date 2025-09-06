cf0353e1d846ffe503403f9c072623795949a412
kunalvvs <kunalvvs@gmail.com>
2025-09-06 10:51:57 +0530

    feat: Update GSAP animations and adjust layout for improved presentation

--- GSAP/Tutorial/script.js
+++ GSAP/Tutorial/script.js
@@ -2,7 +2,7 @@ const box = document.querySelector('.box');
 
 gsap.to(box, {
     duration: 2,
-    x: 300,
+    x: 1400,
     rotation: 360,
     scale: 1.5,
     backgroundColor: '#e74c3c',
@@ -11,3 +11,12 @@ gsap.to(box, {
     repeat: -1,
     yoyo: true
 });
+
+
+gsap.from("h1",{
+    opacity:0,
+    duration:2,
+    y:30,
+    stagger:1,
+    delay:1
+})
